4fc7ef817fd24401b0a1b53e35f7410d27618c40
YOUR_NAME <YOUR_EMAIL>
2025-02-04 18:16:50 -0600

    This is my solution for the Factorial assignment.

--- factorial.js
+++ factorial.js
@@ -1,5 +1,10 @@
-function factorial(num) {
-  return 0;
+function factorial(num){
+  if (num < 0) return "Factorials can't be negative"
+  n = 1;
+  while (num > 0){
+    n = n * num;
+    num = (num - 1);
+  }
+  return n
 }
-
 module.exports = factorial;
